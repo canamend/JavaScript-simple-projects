a3f4e87d696cd7cbe8bd62693234b3304b2b0bf6
canamend <isaac.canamend@gmail.com>
2023-08-05 18:15:42 -0500

    Optimizacion de promesas no secuenciales

--- 06-advanced/main.js
+++ 06-advanced/main.js
@@ -6,6 +6,9 @@ import { callbacksComponent } from './src/concepts/02-callbacks';
 import { promiseComponent } from './src/concepts/03-promises';
 import { promiseRaceComponent } from './src/concepts/04-promise-race';
 import { asyncComponent } from './src/concepts/05-async';
+import { asyncAwaitComponent } from './src/concepts/06-async-await';
+import { asyncAwait2Component } from './src/concepts/07-async-await';
+import { forAwaitComponent } from './src/concepts/08-for-await';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -30,4 +33,7 @@ const element = document.querySelector('.card');
 // callbacksComponent( element );
 //promiseComponent( element );
 // promiseRaceComponent( element );
-asyncComponent( element );
+// asyncComponent( element );
+// asyncAwaitComponent( element );
+// asyncawait2component( element );
+forAwaitComponent( element );
